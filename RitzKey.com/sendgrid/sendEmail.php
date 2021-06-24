5d8028cf00b28ce2a56c12328bea8b4c52839cae
George-Jenkins <george.l.jenkins@live.com>
2021-06-24 11:59:02 -0400

    Update sendEmail.php

--- RitzKey.com/sendgrid/sendEmail.php
+++ RitzKey.com/sendgrid/sendEmail.php
@@ -4,7 +4,7 @@ $logo = "<div style='background-color:#003;padding-left:5px;padding-top:5px;padd
 <img src='http://ritzkey.com/pics/ritzkey-logo5.png' style='height:68px;width:auto;'/>
 </div><p>";
 
-$sendgrid = new SendGrid('admin@abstracthealth.com','wonder');	
+$sendgrid = new SendGrid('admin@abstracthealth.com','********');	
 	$mail = new SendGrid\Mail();
 $mail->
   addTo($email)->
